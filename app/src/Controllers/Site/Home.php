382a8cb3c32846b63e75747e19a87f429f1092c0
Josh Sephton <josh.sephton@gmail.com>
2015-07-19 21:12:24 +0100

    Stubbed out API controller

--- app/src/Controllers/Site/Home.php
+++ app/src/Controllers/Site/Home.php
@@ -1,11 +1,36 @@
 <?php namespace App\Controllers\Site;
 
+use App\Framework\Contracts\Request;
 use App\Framework\View;
 
 
 class Home {
 
 
+	/**
+	 * App\Framework\Contracts\Request $request
+	 */
+	protected $request;
+
+
+	/**
+	 * Construct
+	 *
+	 * @param App\Framework\Contracts\Request $request
+	 * @return void
+	 */
+	public function __construct(Request $request) {
+
+		$this->request = $request;
+
+	}
+
+
+	/**
+	 * List all the Apps
+	 *
+	 * @return void
+	 */
 	public function index() {
 
 		$view = new View('index');
