dff09c538507b494d02e797b74c605fe116dd98f
Josh Sephton <josh.sephton@gmail.com>
2015-07-19 15:45:48 +0100

    Route must be created with method, path and config

--- src/Framework/Route.php
+++ src/Framework/Route.php
@@ -6,7 +6,41 @@ use App\Framework\Contracts\Route as RouteContract;
 class Route implements RouteContract {
 
 
-	//
+	/**
+	 * string $method
+	 */
+	protected $method = 'GET';
+
+
+	/**
+	 * array $path
+	 */
+	protected $path = [];
+
+
+	/**
+	 * array $config
+	 */
+	protected $config;
+
+
+	/**
+	 * Construct
+	 *
+	 * @return void
+	 */
+	public function __construct($method, $path, array $config) {
+
+		$this->method = $method;
+
+		$path = $path;
+		$path = trim($path, '/');
+		$path = explode('/', $path);
+		$this->path = $path;
+
+		$this->config = $config;
+
+	}
 
 
 }
