0d9ded62f4f8f36bb9d94f648e9ea3ebbb695ee2
Josh Sephton <josh.sephton@gmail.com>
2015-07-19 17:25:09 +0100

    Added basic index view

---
+++ src/Views/includes/header.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<!--[if lt IE 7]> <html class="no-js lt-ie10 lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie10 lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie10 lt-ie9" lang="en"> <![endif]-->
+<!--[if IE 9]>    <html class="no-js lt-ie10" lang="en"> <![endif]-->
+<!--[if gt IE 9]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'>
+
+	<title></title>
+
+	<link rel="icon" type="image/vnd.microsoft.icon" href="" />
+	<link rel="apple-touch-icon" type="image/png" href=""/>
+	<meta name="apple-mobile-web-app-title" content="">
+
+	<!-- <meta name="apple-itunes-app" content=""> -->
+	<meta name="application-name" content=""/>
+	<meta name="msapplication-TileColor" content="#ffffff"/>
+	<meta name="msapplication-TileImage" content=""/>
+
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+
+</head>
+
+<body>
