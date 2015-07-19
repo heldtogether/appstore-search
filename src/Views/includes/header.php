9b184a3bc66db1ff4c34bcf91ba5ae79a154a903
Josh Sephton <josh.sephton@gmail.com>
2015-07-19 18:20:40 +0100

    Prepare build process

--- src/Views/includes/header.php
+++ src/Views/includes/header.php
@@ -23,6 +23,8 @@
 	<meta name="description" content="">
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 
+	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+
 </head>
 
 <body>
