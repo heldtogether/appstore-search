033b7f0d0f5bae166667e72cb8db8222bfc18c13
Josh Sephton <josh.sephton@gmail.com>
2015-07-19 16:31:39 +0100

    Pass array of routes into the Application

--- app/src/Framework/Application.php
+++ app/src/Framework/Application.php
@@ -9,15 +9,34 @@ class Application implements ApplicationContract {
 
 
 	/**
+	 * array $routes
+	 */
+	protected $routes;
+
+
+	/**
+	 * Construct
+	 *
+	 * @param array $routes
+	 * @return void
+	 */
+	public function __construct(array $routes) {
+
+		$this->routes = $routes;
+
+	}
+
+
+	/**
 	 * Run the Application
 	 *
 	 * @return App\Framework\Contracts\Response
 	 */
 	public function run() {
 
-		$router = new Router([]);
+		$router = new Router($this->routes);
 
-		$request = new Request;
+		$request = new Request();
 		$reponse = $router->dispatch($request);
 
 		return new Response();
